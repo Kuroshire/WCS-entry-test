6587f9bfd168cf7da4247aa173d88effc0e0eb10
Kuroshire <hugo.bounichou@gmail.com>
2022-10-24 21:33:45 +0200

    un test encore

--- index.cjs
+++ index.cjs
@@ -23,11 +23,11 @@ app.use(express.urlencoded());
 
 app.use("/api/auth", authRoute);
 
-
+/*
 app.use(express.static(path.resolve(__dirname, '.')));
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
-});
+    res.sendFile(path.resolve(__dirname, './index.html'));
+});*/
 
 mongoose
     .connect(process.env.MONGO_URI)
